0135114774b827937a8e93c66ee8e50a0a7885d0
AhmedShaker21 <ahmedshaker23002@gmail.com>
2024-08-04 15:28:54 +0300

    gitText

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailsFooterComponent } from "./details-footer/details-footer.component";
+import { CopyRightComponent } from "./copy-right/copy-right.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, DetailsFooterComponent, CopyRightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
